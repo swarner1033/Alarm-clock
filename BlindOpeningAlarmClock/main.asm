4a758f001d98a7a267b8bcec85a4cb29122f82f8
Sophie Warner <sophiewarner1033@gmail.com>
2021-09-02 16:39:47 +1000

    feat(motor): spin motor with h bridge

--- BlindOpeningAlarmClock/main.asm
+++ BlindOpeningAlarmClock/main.asm
@@ -3,16 +3,14 @@
 .list
 
 .def t0 = r16
-.def t1 = r17
 
-
-d3_on:
-	in	t1, PIND	;load in current value of PIND
-	ldi t0, (1<<PORTD3)	;set bit for pin 3
-	or	t0, t1
-	out DDRD, t0	;set direction of pins 
-	ldi t0, (1<<PORTD3)
+start:
+;Turn on PD3 and PD5
+	ldi t0, (1<<PORTD3) | (1<<PORTD5)
+	out DDRD, t0
+	ldi t0, (0<<PORTD3) | (1<<PORTD5)
 	out PORTD, t0
+
 
 end:
 	rjmp end

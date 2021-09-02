c326c89a033cef0576f616493b8031e7b285261e
Sophie Warner <sophiewarner1033@gmail.com>
2021-09-02 16:08:40 +1000

    feat(led): wirte to DDR without overwriting existing values

--- BlindOpeningAlarmClock/main.asm
+++ BlindOpeningAlarmClock/main.asm
@@ -3,20 +3,15 @@
 .list
 
 .def t0 = r16
+.def t1 = r17
 
-;Turn on PB4
-	ldi t0, (1<<PORTB4)
-	out DDRB, t0
-	out PORTB, t0
 
-;Turn on PC4
-	ldi t0, (1<<PORTC4)
-	out DDRC, t0
-	out PORTC, t0
-
-;Turn on PD4
-	ldi t0, (1<<PORTD4)
-	out DDRD, t0
+d3_on:
+	in	t1, PIND	;load in current value of PIND
+	ldi t0, (1<<PORTD3)	;set bit for pin 3
+	or	t0, t1
+	out DDRD, t0	;set direction of pins 
+	ldi t0, (1<<PORTD3)
 	out PORTD, t0
 
 end:

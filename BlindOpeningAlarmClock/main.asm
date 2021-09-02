2974a953a9d228ebf2c57228cb4b429f7a35326c
Sophie Warner <sophiewarner1033@gmail.com>
2021-09-02 15:28:06 +1000

    feat(led): turn on external LEDs

--- BlindOpeningAlarmClock/main.asm
+++ BlindOpeningAlarmClock/main.asm
@@ -1,7 +1,23 @@
+.nolist
 .include "m328Pdef.inc"
+.list
 
-start:
-	ser r16
-	out PORTB, r16
-halt:
-	rjmp halt
+.def t0 = r16
+
+;Turn on PB4
+	ldi t0, (1<<PORTB4)
+	out DDRB, t0
+	out PORTB, t0
+
+;Turn on PC4
+	ldi t0, (1<<PORTC4)
+	out DDRC, t0
+	out PORTC, t0
+
+;Turn on PD4
+	ldi t0, (1<<PORTD4)
+	out DDRD, t0
+	out PORTD, t0
+
+end:
+	rjmp end
